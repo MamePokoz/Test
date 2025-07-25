17709db33f6233fee6e026c066898c5e76b04a7f
MamePokoz <m0946037250@gmail.com>
2025-07-25 19:14:11 +0700

    Resolved remaining merge conflicts in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // tailwind.config.js
 module.exports = {
   content: ["./app/**/*.{js,ts,jsx,tsx}"],
@@ -29,23 +28,3 @@ tailwind.config = {
     extend: {}
   }
 }
-=======
-// tailwind.config.js
-module.exports = {
-  content: ["./app/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {
-      animation: {
-        'fade-in-up': 'fadeInUp 1s ease-out',
-      },
-      keyframes: {
-        fadeInUp: {
-          '0%': { opacity: 0, transform: 'translateY(20px)' },
-          '100%': { opacity: 1, transform: 'translateY(0)' },
-        },
-      },
-    },
-  },
-  plugins: [],
-};
->>>>>>> 9be121d1490086dfb5ca0046d9b9c4648191c034
